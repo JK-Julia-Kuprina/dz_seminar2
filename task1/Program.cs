cc817c118db004cab21b899bf8b529b5710402cf
Julia Kuprina <131903409+JK-Julia-Kuprina@users.noreply.github.com>
2023-05-24 19:01:26 +0300

    2-я цифра 3-х значного числа

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа. Через строку решать нельзя.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+System.Console.WriteLine("введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 || num >= 1000)
+{
+    System.Console.WriteLine("false");
+    return;
+}
+System.Console.WriteLine(num / 10 % 10);
+
