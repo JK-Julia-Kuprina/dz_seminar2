3e6f410d196d73012f8ce93fd8138330bd4e4d81
Julia Kuprina <131903409+JK-Julia-Kuprina@users.noreply.github.com>
2023-05-29 00:49:49 +0300

    dz1

---
+++ dz_seminar3_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Через строку решать нельзя.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+System.Console.WriteLine("введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num / 1000 == num % 100)
+{
+    System.Console.WriteLine("yes");
+}
+else
+{
+    System.Console.WriteLine("no");
+}
