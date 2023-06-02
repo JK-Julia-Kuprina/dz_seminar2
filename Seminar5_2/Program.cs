532191f22c3e7c9caa0dec06eae786d894b05c5e
Julia Kuprina <131903409+JK-Julia-Kuprina@users.noreply.github.com>
2023-06-02 12:41:28 +0300

    3 task

---
+++ Seminar5_2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные,
+// и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+        Console.Write($"{item} ");
+    System.Console.WriteLine();
+}
+
+void Change1(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++) array[i] = - array[i];
+
+}
+int[] array = new int[12];
+FillArray(array);
+PrintArray(array);
+Change1(array);
+PrintArray(array);
