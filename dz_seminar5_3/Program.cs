5e1157e83bed90d9b61330f924acd0fb0d78e692
Julia Kuprina <131903409+JK-Julia-Kuprina@users.noreply.github.com>
2023-06-07 19:56:30 +0300

    массив вещественных чисел + разница между мак и мин значением

---
+++ dz_seminar5_3/Program.cs
@@ -0,0 +1,44 @@
+﻿//  Задайте массив вещественных чисел. Найдите разницу между максимальным
+// и минимальным элементов массива.
+
+void FillArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        double num = new Random().NextDouble() * 10;
+        array[i] = num;
+        Console.Write(array[i] + ". ");
+        System.Console.WriteLine();
+    }
+}
+
+double Max(double[] array)
+{
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+        if (array[i] > max) 
+        {
+            max = array[i];
+        }
+    return max;
+}
+
+double Min(double[] array)
+{
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    return min;
+}
+
+double[] array = new double[5];
+FillArray(array);
+Max(array);
+Min(array);
+System.Console.WriteLine($"максимальное значение в массиве: {Max(array)}");
+System.Console.WriteLine($"Минимальное значение в массиве: {Min(array)}");
+System.Console.WriteLine($"{Max(array) - Min(array)}:f1");
+
