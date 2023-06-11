642be98345e9f6ba7316be93514020de865319bf
Julia Kuprina <131903409+JK-Julia-Kuprina@users.noreply.github.com>
2023-06-11 16:35:04 +0300

    ввод поьзователем чисел, показать сколько больше 0

---
+++ dz_seminar6_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+        Console.Write($"{item} ");
+    System.Console.WriteLine();
+}
+
+int PositivNumber(int[] array)
+{
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    count++;   
+}
+return count;
+}
+
+Console.Clear();
+
+System.Console.Write("Введите некоторое колличество чисел через пробел ");
+int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+PrintArray(array);
+PositivNumber(array);
+int count = PositivNumber(array);
+
+System.Console.WriteLine(PositivNumber(array));
