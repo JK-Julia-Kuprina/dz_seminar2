a8759677a87e741d16e4b02c5e547a623f4fabc3
Julia Kuprina <131903409+JK-Julia-Kuprina@users.noreply.github.com>
2023-06-18 13:42:35 +0300

    среднее арифметическое элементов в каждом столбце двумерного массива

---
+++ dz_seminar7_3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 21);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}    ");
+        System.Console.WriteLine();
+    }
+}
+
+void Avarage(int[,] array)
+{
+    double sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+                sum = sum + array[i, j];
+    Console.Write($"{sum /(array.GetLength(0)):F1};  ");
+    }
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+Avarage(array);
+
+
